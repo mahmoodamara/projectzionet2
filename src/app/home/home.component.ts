b8a1916e85dd99d123b7a1f4a3aff136eaca70c2
Mahmoud Mustafa <101013620+mahmoodamara@users.noreply.github.com>
2022-07-12 12:50:43 +0300

    adding chain in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../models/message.mode';
+import { MessageService } from '../services/message.service';
+import { UserserviceService } from '../services/userservice.service';
 
-  
+
 
 @Component({
   selector: 'app-home',
@@ -9,12 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 path: String = "src/assets/images/arrow.png"
+userActiv:any[]=[];
+name :string;
+email:string=localStorage.getItem('token');
+userMessages:Message = new Message();
+showmassege:boolean = false;
 
-  constructor() {
+
+
+
+  constructor(private userservice:UserserviceService, private messageservice:MessageService) {
 
    }
 
   ngOnInit(): void {
+    this.getUser();
   }
 
+  getUser(){
+    this.userservice.getUser().subscribe(res=>{
+      this.userActiv=res;
+      this.name=this.userActiv[0].username;
+  })
+  }
+  postMessage(){
+    this.userMessages.userName= this.name;
+    this.userMessages.userEmail=this.email;
+    this.userMessages.adminMessage='';
+
+     this.messageservice.PostMessage(this.userMessages).subscribe(res=>{
+      this.showmassege = true;
+      setTimeout(()=>{
+        this.showmassege = false;
+    }, 3000);
+     })
+  }
 }
